9e67d4438afe69302d0bdfc117731376474a1c6e
Trollerolle <tjtr63311@edu.ucl.dk>
2024-04-09 15:29:03 +0200

    Uge 15

---
+++ Uge 15/Enums/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Enums
+{
+	internal class Program
+	{
+		enum chess 
+		{ 
+			pawns = 0,
+			bishops = 1,
+			rook = 2,
+			knight = 3,
+			queen = 4,
+			king = 5,
+		}
+
+
+		static void Main(string[] args)
+		{
+			chess piece = chess.queen;
+		switch(piece)
+			{
+				case chess.bishops:
+				Console.WriteLine("Du har en løber");
+					break;
+				case chess.rook:
+					Console.WriteLine("Du har et tårn");
+					break;
+				case chess.pawns:
+					Console.WriteLine("Du har en bønde");
+					break;
+				case chess.knight:
+					Console.WriteLine("Du har en springer");
+					break;
+				case chess.queen:
+					Console.WriteLine("Du har en dronning");
+					break;
+				case chess.king:
+					Console.WriteLine("Du har en konge");
+					break;
+			}
+
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Uge 15/Uge 15/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Uge_15
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/* string[] names = { "Anders", "Søren", "Kenneth", "Mark", "Sebastian" };
+
+			for (int i = 0; i < names.Length; i++)
+			{
+				Console.WriteLine(names[i]);
+			}
+			foreach (string name in names) 
+			{ 
+				Console.WriteLine(name);
+			}
+			*/
+
+			List<Friend> friends = new List<Friend>();
+
+			/* 
+			 * friends.Add(new Friend("Fido"));
+			friends.Add(new Friend("Sofus"));
+
+			Console.WriteLine("Skriv navnet på den nye ven");
+			friends.Add(new Friend(Console.ReadLine()));
+			*/
+			string linie = "";
+
+			while (1 == 1)
+			{
+				
+				linie = Console.ReadLine();
+				if (linie == "slut")
+					break;
+				friends.Add(new Friend(linie));
+				Console.Clear();
+				//for (int i = 0; i < friends.Count; i++)
+				//{
+				//	Console.WriteLine(friends[i].name);
+				//}
+				foreach (Friend friend in friends)
+				{
+					Console.WriteLine(friend.name);
+				}
+			}
+			Console.ReadLine();
+		}
+
+		class Friend 
+		{
+			public string name;
+
+			public Friend(string name)
+			{
+				this.name = name;
+			}	
+		}
+
+	}
+}
