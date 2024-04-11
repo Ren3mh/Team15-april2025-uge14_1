cdf1cccbab79a63d0ef4cd63ec995bdae9bf5a4a
Trollerolle <tjtr63311@edu.ucl.dk>
2024-04-11 09:50:57 +0200

    Disaheim

---
+++ Uge 15/Disaheim/Disaheim/Utility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+	public class Utility
+	{
+		public Utility()
+		{
+		
+		}
+
+		public Utility(Book book) 
+		{ 
+			
+		}
+		public Utility(Amulet amulet)
+		{
+
+		}
+
+		public static Double GetValueOfBook(Book book)
+		{
+            return book.Price;
+		}
+		public static double GetValueOfAmulet(Amulet amulet)
+		{
+			if (amulet.Quality == Level.low)
+				return 12.5;
+			else if (amulet.Quality == Level.medium)
+				return 20.0;
+			else if (amulet.Quality == Level.high)
+				return 27.5;
+			else
+				return 20.0;
+		}
+	}
+}
